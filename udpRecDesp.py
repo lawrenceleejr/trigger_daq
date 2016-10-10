3ea674768a15e9d10e87cae1e4df63bb4b2da3a4
sezata <sezata@gmail.com>
2016-10-10 19:01:07 -0400

    fixed udp receiving

--- udpRecDesp.py
+++ udpRecDesp.py
@@ -1,4 +1,4 @@
-import sys, signal
+import sys, signal, struct, time
 from udp import udp_fun
 
 global bufsize
@@ -8,40 +8,50 @@ maxpkt = 1024
 global UDP_PORT
 UDP_PORT = 6008
 global UDP_IP
-UDP_IP = "192.168.0.10"
+UDP_IP = ""
 global timeout
 timeout = 5
+global readInterval
+readInterval = 0.01
 def main():
     try:
         udp = udp_fun()
         wordcount = 0
         print "Receiving from TP"
+        print "Doesn't delete old file!"
         print "Ctrl+C to stop!"
         udp.set_udp_port(UDP_PORT)
         udp.set_udp_ip(UDP_IP)
         while True:
             data, addr = udp.udp_client(maxpkt,bufsize)
-            print "received from " + addr
+#            print "received from ", addr
             count = 1
             while (count < timeout * 1/readInterval):
                 #udpPacket = data
-                udpPacket = data.split()
+                udpPacket = [data]
+#                print udpPacket
                 # this assumes we receive the data in packets of 4 bytes, or 32 bits
-                header = int(udpPacket[0],16)
-                #splitPacket = [udpPacket[i:i+8] for i in range(0, len(udpPacket), 8)] #groups into 8
-                if not udpPacket:
-                    addrnum = header[6:] # address number reading from
-                    with open("mmtp_test_%d.dat"%(addrnum), "a") as myfile:
-                        for i in xrange(1, len(udpPacket), 1):
-                            word = int(udpPacket[i], 16)
+                datalist = [format(int(hex(ord(c)), 16), '02X') for c in list(udpPacket[0])]
+#                print datalist
+                if len(udpPacket)>0:
+                    addrnum = datalist[7] # address number reading from
+#                    print "addr", int(addrnum)
+                    del datalist[:9]
+#                    print datalist
+                    wordcount = 0
+                    with open("mmtp_test_%d.dat"%(int(addrnum)), "a") as myfile:
+                        wordout = ''
+                        for byte in datalist:
                             wordcount = wordcount + 1
-                            if (wordcount%4==0):
-                                myfile.write(word + '\n')
-                            else:
-                                myfile.write(word)
+                            wordout = wordout + byte
+                            if wordcount == 16:
+#                                print "WORDOUT", wordout
+                                myfile.write(str(wordout) + '\n')
+                                wordout = ''
+                                wordcount = 0
                     myfile.close()
                     count = 1
-                sleep(0.01)
+                time.sleep(0.01)
                 count = count + 1
     except KeyboardInterrupt:
         print "Stopped!"
