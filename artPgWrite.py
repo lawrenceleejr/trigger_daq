996e9165b5703e26a23984ec43ead9f7316df38b
sezata <sezata@gmail.com>
2016-10-26 11:50:26 -0400

    artpacket writing to fifo

---
+++ artPgWrite.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python 
+
+# Adapted from artPgWrite.m
+# Writes 32 bit words and writes them to the TP FIFOs
+
+# Input data format: Column 1: 32 bit data word Column 2: FIFO reg address
+
+# A.Wang, last edited Oct. 26, 2016
+
+import sys,socket,struct,getopt
+
+TCP_IP = "192.168.1.10"
+TCP_PORT = 7
+
+def main(argv):
+    inputfile = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:", ["i="])
+    except getopt.GetoptError:
+        print 'artPgWrite.py -i <inputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'artPgWrite.py -i <inputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            print arg
+            inputfile = arg
+
+    # Opening file and sending
+    datafile = open(inputfile, 'r')
+    opensock = open_tcp(TCP_IP,TCP_PORT)
+    for rawline in datafile:
+        line = rawline.split(" ")
+        pktData = str(line[0])
+        fifoAddr = str(line[1])
+        #print pktData, fifoAddr
+        send_data(opensock,addr,pkt)
+        sleep(0.3)
+
+def open_tcp(ip,port):
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.connect((ip,port))
+    return sock
+
+def send_data(sock,addr,pkt):
+    header = "abcd1234"
+    type = "FE170002"
+    addr = "000000"+addr
+    msg = [socket.htonl(int(msg1,16)),socket.htonl(int(msg2,16)),socket.htonl(int(addr,16)),socket.htonl(int(pkt,16))]
+    msg = struct.pack("<4I",*msg)
+    sock.sendall(msg)
+    sock.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
