12f5033504109660390a3f82a5adfd9cac0c40cf
lawrenceleejr <lawrenceleejr@gmail.com>
2016-11-21 20:51:14 +0100

    adding prelim event display and json output to decode function

--- decodeGBT_32bit.py
+++ decodeGBT_32bit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # Decoded output format for GBT packets
-# 16 bytes for every "strobe" - 128 bits 
+# 16 bytes for every "strobe" - 128 bits
 # data: 12 bits of BCID + 8 bits of ERR_FLAGS + 32 bits of HITLIST + 8 bits of art data parity + 8 * 6 bits of art data
 # constant C,E written out somewhere
 
@@ -9,7 +9,8 @@
 
 
 import sys, getopt,binstr
-
+import json
+import collections
 
 def main(argv):
     remapflag = 0
@@ -34,6 +35,10 @@ def main(argv):
     datafile = open(inputfile, 'r')
     decodedfile = open(outputfile, 'w')
 
+
+    timestamp = ""
+    outputData = collections.OrderedDict()
+
     eventnum = 0
     n = 5 #starting pt of data
     lines = []
@@ -42,11 +47,13 @@ def main(argv):
     for line in datafile:
         if str(line[0:4]) =='TIME':
             decodedfile.write('\n'+line)
+            timestamp = line.rstrip()
+            outputData[timestamp] = []
             continue
         lines.append(line[:len(line)-1])
         if len(lines) == 4:
             strobe = ''.join(map(str,lines))
-            artdata = strobe[n+15:n+27] 
+            artdata = strobe[n+15:n+27]
             parity = strobe[n+13:n+15]
             hitmap = strobe[n+5:n+13]
             hitmap = "{0:032b}".format(int(hitmap,16))
@@ -72,22 +79,32 @@ def main(argv):
                         vmmlist.append((31-i)%8)
             decodedfile.write( "BCID: {: >10}  Hitmap: {: >20}  Boards: ".format(int(bcid,16), hitmap ) )
             reversedBoardList = reversed(boardlist)
-            reversedBoardList = [str(x) for x in reversedBoardList]
-            decodedfile.write( '{: >30}'.format( " ".join( reversedBoardList ) if len(reversedBoardList) else "N/A" ) )
+            reversedBoardStringList = [str(x) for x in reversedBoardList]
+            decodedfile.write( '{: >30}'.format( " ".join( reversedBoardStringList ) if len(reversedBoardStringList) else "N/A" ) )
 
             decodedfile.write('  VMMs:')
             reversedVMMList = reversed(vmmlist)
-            reversedVMMList = [str(x) for x in reversedVMMList]
-            decodedfile.write( '{: >30}'.format( " ".join( reversedVMMList ) if len(reversedVMMList) else "N/A" ) )
+            reversedVMMStringList = [str(x) for x in reversedVMMList]
+            decodedfile.write( '{: >30}'.format( " ".join( reversedVMMStringList ) if len(reversedVMMStringList) else "N/A" ) )
 
             reversedVMMData = reversed(vmmdata)
-            reversedVMMData = [str(x) for x in reversedVMMData]
-            decodedfile.write( '  CH: {: >30}\n'.format( " ".join( reversedVMMData ) if len(reversedVMMData) else "N/A" ) )
+            reversedVMMStringData = [str(x) for x in reversedVMMData]
+            decodedfile.write( '  CH: {: >30}\n'.format( " ".join( reversedVMMStringData[:len(vmmlist)] ) if len(vmmlist) else "N/A" ) )
+            ### here the list is truncated.. make sure that's what I'm supposed to do...
+
+            outputData[timestamp].append(
+                                            ( int(bcid,16),
+                                              zip(boardlist[::-1], vmmlist[::-1],vmmdata[::-1])
+                                            )
+                                        )
+
+    with open(outputfile.split(".")[0]+".json", 'w') as outputJSONFile:
+        json.dump(outputData, outputJSONFile)
 
     decodedfile.close()
     datafile.close()
     print "done decoding, exiting \n"
-    
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
