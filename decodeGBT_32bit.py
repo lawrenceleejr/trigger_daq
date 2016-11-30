c73463d201f068b566084489987eedd54bb2ee07
lawrenceleejr <lawrenceleejr@gmail.com>
2016-11-30 13:46:34 +0100

    adding back in pretty output for decodeGBT

--- decodeGBT_32bit.py
+++ decodeGBT_32bit.py
@@ -73,33 +73,25 @@ def main(argv):
                     if (hitmap[i] is "1"):
                         boardlist.append((31-i)/8)
                         vmmlist.append((31-i)%8)
-            decodedfile.write('BCID: ' + str(int(bcid,16)) + '\thitmap: ' + hitmap + '\tBoards:')
-            if len(boardlist) is 0:
-                decodedfile.write(' N/A')
-            for i in reversed(range(len(boardlist))):
-                decodedfile.write(' ' + str(boardlist[i]))
-            decodedfile.write('\tVMMs:')
-            for i in reversed(range(len(vmmlist))):
-                decodedfile.write(' ' + str(vmmlist[i]))
-            if len(vmmlist) is 0:
-                decodedfile.write(' N/A\n')
-                continue
-            if len(vmmlist) is 1:
-                decodedfile.write('\t')
-            decodedfile.write('\tCHs ' + str(vmmdata[7])\
-                              + ' ' + str(vmmdata[6])\
-                              + ' ' + str(vmmdata[5])\
-                              + ' ' + str(vmmdata[4])\
-                              + ' ' + str(vmmdata[3])\
-                              + ' ' + str(vmmdata[2])\
-                              + ' ' + str(vmmdata[1])\
-                              + ' ' + str(vmmdata[0])\
-                              + '\n')
+
+            decodedfile.write( "BCID: {: >10}  Hitmap: {: >20}  Boards: ".format(int(bcid,16), hitmap ) )
+            reversedBoardList = reversed(boardlist)
+            reversedBoardStringList = [str(x) for x in reversedBoardList]
+            decodedfile.write( '{: >30}'.format( " ".join( reversedBoardStringList ) if len(reversedBoardStringList) else "N/A" ) )
+
+            decodedfile.write('  VMMs:')
+            reversedVMMList = reversed(vmmlist)
+            reversedVMMStringList = [str(x) for x in reversedVMMList]
+            decodedfile.write( '{: >30}'.format( " ".join( reversedVMMStringList ) if len(reversedVMMStringList) else "N/A" ) )
+
+            reversedVMMData = reversed(vmmdata)
+            reversedVMMStringData = [str(x) for x in reversedVMMData]
+            decodedfile.write( '  CH: {: >30}\n'.format( " ".join( reversedVMMStringData[:len(vmmlist)] ) if len(vmmlist) else "N/A" ) )
+            ### here the list is truncated.. make sure that's what I'm supposed to do...
 
             tmpKey = str(int(bcid,16))
             if tmpKey in outputData:
                 tmpKey = tmpKey+"_1"
-            print tmpKey
             outputData[tmpKey] = zip(boardlist[::-1], vmmlist[::-1],vmmdata[::-1])
 
 
