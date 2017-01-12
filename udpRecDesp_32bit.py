a3ad019d6191b25f4e7c365c192f263800b0e647
lawrenceleejr <lawrenceleejr@gmail.com>
2017-01-12 16:23:59 +0100

    adding functionality to udpRecDesp_32bit.py to create new output file if desired

--- udpRecDesp_32bit.py
+++ udpRecDesp_32bit.py
@@ -6,10 +6,13 @@
 import sys, signal, struct, time
 from udp import udp_fun
 
+from optparse import OptionParser
+import os.path
+
 #bufsize = 65536
 bufsize = 1000000
 maxpkt = 1024*4
-UDP_PORT = 
+UDP_PORT = 6008
 UDP_IP = ""
 readInterval = 1
 sleeptime = 0.01
@@ -18,31 +21,50 @@ sleeptime = 0.01
 timeflag = False
 
 def main():
+
+    parser = OptionParser(usage="usage: %prog [options] outputFileName")
+    parser.add_option("-n", "--newFile",
+                      action="store_true",
+                      dest="newFile",
+                      default=False,
+                      help="if the output file already exists don't overwrite it")
+    (options, args) = parser.parse_args()
+
+    outputFileName = "mmtp_test.dat" if len(args)==0 else args[0]
+
+    if options.newFile:
+        counter = 1
+        while os.path.exists(outputFileName):
+            outputFileName = outputFileName.split("__")[0]+"__%d"%counter
+            counter += 1
+
     try:
         udp = udp_fun()
         wordcount = 0
         print "Receiving from TP"
-        print "Doesn't delete old file!"
+        if not options.newFile:
+            print "Doesn't delete old file! If new file desired, add -n option."
         print "Ctrl+C to stop!"
         print "Sleeping for: ", sleeptime
         udp.set_udp_port(UDP_PORT)
         udp.set_udp_ip(UDP_IP)
         rawsock = udp.udp_client(maxpkt,bufsize)
-        while True:
-            data, addr = udp.udp_recv(rawsock)
-#            print "received from ", addr
-#            print data
-            udpPacket = [data]
-            # this assumes we receive the data in packets of 4 bytes, or 32 bits
-            datalist = [format(int(hex(ord(c)), 16), '02X') for c in list(udpPacket[0])]
-            if len(udpPacket)>0:
-                addrnum = datalist[7] # address number reading from
-                # print "addr", int(addrnum)
-                del datalist[:8]
-                # print datalist
-                wordcount = 0
-                header = [0,0,0,0] #20,21,22,23
-                with open("mmtp_test_%d.dat"%(int(addrnum)), "a") as myfile:
+
+        with open(outputFileName, "a") as myfile:
+            while True:
+                data, addr = udp.udp_recv(rawsock)
+    #            print "received from ", addr
+    #            print data
+                udpPacket = [data]
+                # this assumes we receive the data in packets of 4 bytes, or 32 bits
+                datalist = [format(int(hex(ord(c)), 16), '02X') for c in list(udpPacket[0])]
+                if len(udpPacket)>0:
+                    addrnum = datalist[7] # address number reading from
+                    # print "addr", int(addrnum)
+                    del datalist[:8]
+                    # print datalist
+                    wordcount = 0
+                    header = [0,0,0,0] #20,21,22,23
                     wordout = ''
                     for byte in datalist:
                         if byte == 'A2': #finder data
@@ -68,11 +90,12 @@ def main():
                             print wordout
                             wordout = ''
                             wordcount = 0
-                myfile.close()
-                time.sleep(sleeptime)
+                    time.sleep(sleeptime)
+
     except KeyboardInterrupt:
         print "Stopped!"
         rawsock.close()
+        myfile.close()
 
 if __name__ == "__main__":
     main()
