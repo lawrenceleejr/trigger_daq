21306480496cb220d41d3b6c97af4b6bffb65bbe
sezata <sezata@gmail.com>
2017-01-16 11:51:01 -0500

    changed doPackets and artPgWrite to use larry's functions

--- doPackets.py
+++ doPackets.py
@@ -4,7 +4,7 @@
 # Adapted from doPackets.m
 # takes artPgWrite.py and writes a pcket, and then looks at the data
 
-# A.Wang, last edited Jan 9, 2017
+# A.Wang, last edited Jan 16, 2017
 
 # Not yet tested
 
@@ -14,47 +14,11 @@ import multiprocessing as mp
 TCP_IP = "192.168.1.10"
 TCP_PORT = 7
 
-def do(inputfile):
-    # Opening file and sending
-    datafile = open(inputfile, 'r')
-    opensock = open_tcp(TCP_IP,TCP_PORT)
-    print "\n"
-    print "*_*_*_*_*_*_*_*_*_*_*"
-    print "Resetting BCID"
-
-    # reset on
-    artPgWrite.send_data(opensock,"01","00000002")
-    time.sleep(0.1)
-    # reset off
-    artPgWrite.send_data(opensock,"01","00000003")
-    time.sleep(0.1)
-    # reset BCID
-    artPgWrite.send_data(opensock,"01","00000004")
-    time.sleep(0.1)
-
-    print "Sending ART Data"
-    #sending data
-    for rawline in datafile:
-        line = rawline.split(" ")
-        pktData = str(line[0])
-        fifoAddr = str(line[1])
-        print pktData, fifoAddr
-        artPgWrite.send_data(opensock,addr,pkt)
-        time.sleep(0.3)
-    datafile.close()
-
-    print "Starting DAQ"
-    artPgWrite.send_data(opensock,"01","00000001")
-    time.sleep(5)
-    print "Stopping DAQ"
-    artPgWrite.send_data(opensock,"01","00000011")
-    time.sleep(0.1)
-    print "*_*_*_*_*_*_*_*_*_*_*"
-    
-if __name__=="__main__":
+def main(argv):
     inputfile = ''
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hi:", ["i="])
+        opts, args = getopt.getopt(argv, "hi:", ["i="])
     except getopt.GetoptError:
         print 'doPackets.py -i <inputfile>'
         sys.exit(2)
@@ -73,3 +37,46 @@ if __name__=="__main__":
     writepackets.start()
     time.sleep(10)
 #    receivedata.shutdown()
+
+def reset_bcid():
+    # reset on
+    regWrite.writeToRegister(TCP_IP,TCP_PORT,sock,"00000001","00000002")
+    time.sleep(0.1)
+    # reset off
+    regWrite.writeToRegister(TCP_IP,TCP_PORT,sock,"00000001","00000003")
+    time.sleep(0.1)
+    # reset BCID
+    regWrite.writeToRegister(TCP_IP,TCP_PORT,sock,"00000001","00000004")
+    time.sleep(0.1)
+
+def do(inputfile):
+    # Opening file and sending
+    datafile = open(inputfile, 'r')
+    print "\n"
+    print "*_*_*_*_*_*_*_*_*_*_*"
+    print "Resetting BCID"
+
+    reset_bcid()
+
+    print "Sending ART Data"
+    #sending data
+    for rawline in datafile:
+        line = rawline.split(" ")
+        pktData = str(line[0])
+        fifoAddr = str(line[1])
+        fifoAddr = "000000"+fifoAddr
+        print pktData, fifoAddr        
+        regWrite.writeToRegister(TCP_IP,TCP_PORT,fifoAddr,pktData)
+        time.sleep(0.3)
+    datafile.close()
+
+    print "Starting DAQ"
+    regWrite.writeToRegister(TCP_IP,TCP_PORT,"00000001","00000001")
+    time.sleep(5)
+    print "Stopping DAQ"
+    regWrite.writeToRegister(TCP_IP,TCP_PORT,"00000001","00000011")
+    time.sleep(0.1)
+    print "*_*_*_*_*_*_*_*_*_*_*"
+
+if __name__=="__main__":
+    main(sys.argv[1:])
