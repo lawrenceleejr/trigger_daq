acdd921b3beb372eb064d21f9fe47053fcc077a5
Noah Wuerfel <nwuerfel@college.harvard.edu>
2017-04-08 13:15:49 -0400

    getEvent and parseEvent work properly, main still has old, dead code and needs cleaning

--- noah/dispFind.py
+++ noah/dispFind.py
@@ -10,26 +10,66 @@ import matplotlib.patches as patches
 import sys, getopt, binstr
 
 helpMessage = 'usage: <cmdline> python noahFIND.py -i <inputfile> -e <eventNo> -b <bcidNo> \n -v for verbose'
+verbose = False
 
 # get data for specific event number
 # our data is nicely arranged as events always take 10 lines
 # takes eventNum and filepointer
 # TODO worry about efficiency of reads from file (linecache?)
 def getEvent(eventNum, openFile):
-    lineNum = ((eventNum - 1) * 10) + 1
+    lineNum = (eventNum-1) * 10
+    lines = openFile.readlines()
+    eventLines = lines[lineNum:lineNum + 10]
+    if verbose:
+        print "here are the event lines"
+        print eventLines
+
+    event = parseEvent(eventLines)
+    return event
     
+def parseEvent(eventLines):    
+
+    assert(len(eventLines) == 10), 'event longer or shorter than 10 lines'
+
+    event = []
+    tmpLines = []
+
+    for line in eventLines:
+
+        lineInfo = line.split()
+
+        if verbose:
+            print "Processing line: %s" % line
+
+        # handle event lines
+        # later this should save events or something so we can get them efficiently
+        # currently doesn't do that LOL
+        # TODO
+        if lineInfo[0] == "Event":
+            lineEventNum = int(lineInfo[1])
+            continue # TODO store events for the getEvent method
+        # bcid lines
+        elif lineInfo[1] == "BCID:":
+            lineBCID = int(lineInfo[2])
+            continue # TODO store BCID for getEvent
+        else:
+            tmpLines.append([int (x) for x in lineInfo])
+            # each event comprised of 8 things
+            if len(tmpLines) == 8:
+                event = [lineEventNum] + [lineBCID] + tmpLines
+                if verbose:
+                    print "Event %i:" % lineEventNum
+                    print event
+                return event
+
 
 
 # open gui(?) to display events
 # need to give input file
 def main(argv):
     inputfile = ''
-    targetEventNum = 0
-    targetEventReached = True 
-    targetBCID = 0
     tmpLines = []
     events = []
-    verbose = False
     try: 
         opts, args = getopt.getopt(argv, "hvi:e:b:",["ifile=","eventNo=","targetBCID="])
     except getopt.GetoptError:
@@ -40,17 +80,20 @@ def main(argv):
             print helpMessage
             sys.exit()
         elif opt == '-v':
-            verbose = True
+            global verbose
+            verbose = True;
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-e", "--eventNo"):
             targetEventNum = int(arg)
-            targetEventReached = False
 
     if inputfile == '':
         print 'please supply an inputfile, -h for help'
         sys.exit()
     dataFile = open(inputfile, 'r')
+
+    if targetEventNum != 0:
+        event = getEvent(targetEventNum, dataFile)
 
     lines = dataFile.readlines()
     for line in lines:
@@ -66,19 +109,7 @@ def main(argv):
         # TODO
         if lineInfo[0] == "Event":
             lineEventNum = int(lineInfo[1])
-            if verbose:
-                print "targetEventNum: %i, lineEventNum: %i" % (targetEventNum, lineEventNum)
-            if targetEventNum != 0 and lineEventNum != targetEventNum:
-                continue
-            else:
-                targetEventReached = True
             continue # TODO store events for the getEvent method
-
-        if not targetEventReached:
-            if verbose:
-                print "wrong event, skipping"
-            continue
-
         # bcid lines
         elif lineInfo[1] == "BCID:":
             lineBCID = lineInfo[2]
