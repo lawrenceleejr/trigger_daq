a278978f104ed078dfab3f92979e0667bbdb7762
Noah Wuerfel <nwuerfel@college.harvard.edu>
2017-04-10 15:49:27 -0400

    Fixed live display to take raw TP output using Ann's decoding, need to add flags for decoding parameters

--- findDisplay/dispFind.py
+++ findDisplay/dispFind.py
@@ -13,7 +13,18 @@ import matplotlib.patches as patches
 import os, signal, sys, getopt, binstr
 
 helpMessage = 'usage: <cmdline> python dispFIND.py -i <inputfile> -e <eventNo> -b <bcidNo> \n -v for verbose \n -l for live'
+
+#option flags for decoding 
 verbose = False
+remapFlag = 0
+offsetFlag = 0
+octGeo = 1
+
+# fixed vals for decoding 
+remapping = [11,10,9,8,15,14,13,12,3,2,1,0,7,6,5,4,27,26,25,24,31,30,29,28,19,18,17,16,23,22,21,20]
+offsets = reversed(["856","85B","84A","84F","84A","84F","856","85B"])    
+overall_offset = "C00"
+
 
 # get data for specific event number
 # TODO worry about efficiency of reads from file (linecache?)
@@ -79,8 +90,66 @@ def parseEvent(eventLines):
                 event = [lineEventNum] + [lineBCID] + tmpLines
                 return event
 
+# parses raw data from TP FIFO 23
+# most of this is from Ann Wang's decode_FIND32bit
+def parseRawEvent(rawEventLines):
+
+    lines = []
+    hits = []
+    event = [1337]
+
+    for line in rawEventLines:
+        if str(line[0:4]) == 'TIME':
+            # TODO if time info is important?
+            continue
+        lines.append(line[:len(line)-1]) 
+        if len(lines) == 9:
+            header = lines[0]
+            bcid = int(header[4:])
+            event.append(bcid)
+            for i in range(4):
+                hits.append(lines[i+1])
+            iplane = 0
+            for hit in hits:
+                for j in range(2):
+                    ivmm, ich = rawDecode(hit,iplane,j)
+                    iplane = iplane + 1
+                    event.append([ivmm,ich])
+
+    return event
+            
+                
+# straight from Ann's decoder
+# ../decoders/decodeFIND_32bit.py
+# would import it but its a pain in the ass importing from other directories
+def rawDecode(hit, ip, id):
+    strip = 0
+    if offsetFlag == 1:
+        if verbose:
+            print "Added offset!"
+        strip = int(hit[id*4:id*4+4],16)-int(overall_offset,16)-int(offsets[iplane],16)
+    else:
+        strip = int(hit[id*4:id*4+4],16)
+    if strip is not 0:
+#        if ((ip == 2) or (ip == 3) or (ip == 4) or (ip == 6)) and (octGeo == 1):
+        # FTS
+        if ((ip == 0) or (ip == 1) or (ip == 5) or (ip == 7)) and (octGeo == 1):
+            strip = 512-strip-1
+        if remapFlag == 1:
+            ivmm = remapping[strip/64]%8
+        else:
+            ivmm = (strip/64)%8
+            ich = strip%64+1
+    else:
+        ivmm = 0
+        ich = 0
+    return ivmm, ich
+
+
+
 # "live" display of data, just looks at last event recorded in the file after some
 # time
+# the live display takes raw data from FIFO 23 and decodes it 
 # TODO build a "pause" button
 def goLive(fileName):
     numberDisplayed = 1
@@ -88,7 +157,7 @@ def goLive(fileName):
         unused = os.system('clear') # avoid printing return val
         revLines = list(reversed(open(fileName,'r').readlines()))
         lastEventLines = list(reversed(revLines[0:10]))
-        lastEvent = parseEvent(lastEventLines)
+        lastEvent = parseRawEvent(lastEventLines)
         if verbose:
             print lastEvent
             print "now sleeping..."
