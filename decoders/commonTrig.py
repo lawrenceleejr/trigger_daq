e3f45eec919a5850ad71f24b2015f622f035ce8f
sezata <sezata@gmail.com>
2017-05-10 19:44:09 -0400

    fixed offsets for uv

--- decoders/commonTrig.py
+++ decoders/commonTrig.py
@@ -6,8 +6,10 @@ import sys, getopt,binstr, time
 
 class tconsts:
 
-    OFFSETS = ["3A","3A","47","47","40","40","40","40"] # in the finder order
-    HITOFFSETS = ["40","40","47","3A","47","3A","40","40"][::-1] # in the hit packet order
+    OFFSETS = ["47","47","3A","3A","40","40","40","40"] # in the finder order
+#    OFFSETS = ["3A","3A","47","47","40","40","40","40"] # in the finder order
+    HITOFFSETS = ["40","40","3A","47","3A","47","40","40"][::-1] # in the hit packet order
+#    HITOFFSETS = ["40","40","47","3A","47","3A","40","40"][::-1] # in the hit packet order
     OVERALLOFFSET = "000"
     FLIPPEDBOARDS = [0, 1, 5, 7] # in the finder order
     FLIPPEDHITBOARDS = [1, 2, 4, 7] # in the hit order
