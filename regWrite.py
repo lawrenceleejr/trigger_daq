76fc1abd8f406a26a7e3ba7fd6c9f7df74cd2711
alexandertuna <alexander.tuna@gmail.com>
2017-09-05 21:49:06 -0400

    grabbing the timestamp

--- regWrite.py
+++ regWrite.py
@@ -22,8 +22,8 @@ def main(argv):
     ip,port = '192.168.2.10','7'
 
     try:
-        opts, args = getopt.getopt(argv, "hi:p:a:m:rue:", ["ip=","port=","address=", "message=",\
-                                                          "resetGBT","gbtInput","fe=","fifoOutEna=",\
+        opts, args = getopt.getopt(argv, "hi:p:a:m:true:", ["ip=","port=","address=", "message=",\
+                                                          "timestamp", "resetGBT","gbtInput","fe=","fifoOutEna=",\
                                                           "df=",\
                                                           "jtag"])
     except getopt.GetoptError:
@@ -37,6 +37,7 @@ def main(argv):
             print '-m            specifies message sent'
             print '-i            specifies IP'
             print '-p            specifies port'
+            print '-t            prints the timestamp'
             print '-r            resets GBT transciever'
             print '-u            enables gbt input to FIFOs'
             print '-e <0 or 1>   enables all or no FIFO outputs'
@@ -48,6 +49,9 @@ def main(argv):
             ip = arg
         elif opt in ("-p", "--port"):
             port = arg
+        elif opt in ("-t", "--timestamp"):
+            readTimestamp(ip, port)
+            return
         elif opt in ("-a", "--address"):
             address.append(arg)
         elif opt in ("-m", "--message"):
@@ -102,8 +106,45 @@ def main(argv):
 #            os.system("ls")
             os.system("vivado -mode batch -source tcl_scripts/regWrite.tcl -tclargs " + str(a) + " " + str(m))
 
-        
+def readTimestamp(ip, port):
 
+    address = "00000000"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, int(port)))
+    msg1 = "abcd1234"
+    msg2 = "FE170001"
+    msg = [socket.htonl(int(msg1,    16)),
+           socket.htonl(int(msg2,    16)),
+           socket.htonl(int(address, 16)),
+           ]
+    msg = struct.pack("<3I", *msg)
+    sock.sendall(msg)
+
+    data, address = sock.recvfrom(4096)
+    if not data:
+        sys.exit("No data received from timestamp!")
+    datalist = [format(int(hex(ord(c)), 16), '02X') for c in list(data)]
+    sock.close()
+
+    # timestamp is the last 32 bits
+    datastr = "".join(datalist)
+    if len(datastr) < 8:
+        sys.exit("Timestamp data (%s) is less than 8 hex characters -- not good." % (datastr))
+    datatime = datastr[-8:]
+    databin  = format(int(datatime, 16), "032b")
+
+    # https://www.xilinx.com/support/documentation/application_notes/xapp497_usr_access.pdf
+    day   = int(databin[0:5],   2)
+    month = int(databin[5:9],   2)
+    year  = int(databin[9:15],  2)
+    hour  = int(databin[15:20], 2)
+    min   = int(databin[20:26], 2)
+    sec   = int(databin[26:],   2)
+
+    print
+    print "Xilinx time : %s" % (datatime)
+    print "Human time  : %04i-%02i-%02i @ %02i:%02i:%02i" % (year+2000, month, day, hour, min, sec)
+    print
 
 def writeToRegister(ip,port,address,message):
     """Little function that actually takes care of the transmission"""
