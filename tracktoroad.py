3a22809459d0f0bac0c1413fd114ee20a858d485
sezata <sezata@gmail.com>
2018-01-23 12:46:59 -0500

    some bug fixes to tracktoroad + margin info

--- tracktoroad.py
+++ tracktoroad.py
@@ -3,8 +3,9 @@
 import ROOT, sys, math
 
 # offsets in fw
-v_off = 7
-u_off = -6  
+v_off = 71
+u_off = 58  
+x_off = 64
 
 # flipped planes
 flipped = [0, 3, 5, 6]
@@ -17,7 +18,7 @@ def create_roads(nstrips):
     m_roads = []
     for i in range(nroad):
         m_roads.append(Road(i,i,i))
-        nuv = 4; #1, uv factor for 20 cm chamber
+        nuv = 3; #1, uv factor for 20 cm chamber
         for uv in range(1, nuv+1):
             if (i-uv < 0):
                 continue
@@ -29,21 +30,21 @@ def create_roads(nstrips):
             myroad_5 = Road(i, i+uv,   i-uv+1)
             m_roads.append(myroad_0)
             m_roads.append(myroad_1)
-            #m_roads.append(myroad_2)
-            #m_roads.append(myroad_3)
-            m_roads.append(myroad_4)
-            m_roads.append(myroad_5)
+            m_roads.append(myroad_2)
+            m_roads.append(myroad_3)
+            #m_roads.append(myroad_4)
+            #m_roads.append(myroad_5)
     return m_roads
 
 def flip(ch, ib):
     if ib in flipped:
-        return 512 - ch 
+        return 511 - ch 
     else:
         return ch
     
 def offset(strip, ib):
     if ib < 2 or ib > 5:
-        return strip
+        return strip + x_off
     elif ib == 5 or ib == 3:
         return strip + v_off
     else:
@@ -64,6 +65,7 @@ class Road(object):
         self.iroadx = iroadx
         self.iroadu = iroadu
         self.iroadv = iroadv
+        self.margin_boards = []
     def contains_hit(self, ib, strip):
         iroad = -1
         if ib < 2 or ib > 5:
@@ -72,72 +74,78 @@ class Road(object):
             iroad = self.iroadu
         else:
             iroad = self.iroadv
+        strip = offset(strip, ib)
+
         slow = 8 * iroad 
         shigh = 8 * (iroad + 1) 
+        if strip >= slow and strip < shigh:
+            return True
+
         slow -= 0
         shigh += 4
-        strip = offset(strip, ib)
-        return strip >= slow and strip < shigh
+        
+        if strip >= slow and strip < shigh:
+            self.margin_boards.append(ib)
+            return True
 
 def main():
 
+    event = 1572
+
     hits = []
 
+    print "event %d"%(event)
     # event 1310
-    hits.append(Hit(5, 7, 31))
-    hits.append(Hit(2, 0, 15))
-    hits.append(Hit(6, 7, 40))
-    hits.append(Hit(1, 0, 25))
-    hits.append(Hit(0, 7, 40))
+    if event == 1310:
+        # match
+        hits.append(Hit(5, 7, 31))
+        hits.append(Hit(2, 0, 15))
+        hits.append(Hit(6, 7, 40))
+        hits.append(Hit(1, 0, 25))
+        hits.append(Hit(0, 7, 40))
 
-    # event 1799
-    # looks ok
-#     hits.append(Hit(5, 6, 63))
-#     hits.append(Hit(3, 6, 63))
-#     hits.append(Hit(4, 1, 47))
-#     hits.append(Hit(7, 1, 25))
-#     hits.append(Hit(6, 6, 40))
-#     hits.append(Hit(0, 6, 40))
+    if event == 1799:
+        hits.append(Hit(5, 6, 63))
+        hits.append(Hit(3, 6, 63))
+        hits.append(Hit(4, 1, 47))
+        hits.append(Hit(7, 1, 25))
+        hits.append(Hit(6, 6, 40))
+        hits.append(Hit(0, 6, 40))
 
-#     # event 1095 # this is weird b/c it wraps around the boards tho
-#     hits.append(Hit(5, 7, 23))
-#     #hits.append(Hit(3, 7, 40))
-#     hits.append(Hit(4, 0, 7))
-#     hits.append(Hit(2, 0, 7))
-#     #hits.append(Hit(7, 7, 47))
-#     hits.append(Hit(6, 7, 40))
-#     hits.append(Hit(1, 0, 25))
-#     hits.append(Hit(0, 7, 40))
+    if event == 1095:
+        # this is weird b/c it wraps around the boards tho
+        hits.append(Hit(5, 7, 23))
+        # hits.append(Hit(3, 7, 40))
+        hits.append(Hit(4, 0, 7))
+        hits.append(Hit(2, 0, 7))
+        # hits.append(Hit(7, 7, 47))
+        hits.append(Hit(6, 7, 40))
+        hits.append(Hit(1, 0, 25))
+        hits.append(Hit(0, 7, 40))
+    
+    if event == 1123:
+        # i think here there is a duplicate that doesn't make sense, 1126 + 1124
+        hits.append(Hit(3, 6, 23))
+        hits.append(Hit(2, 1, 7))
+        hits.append(Hit(7, 1, 25))
+        hits.append(Hit(1, 1, 25))
+        hits.append(Hit(0, 6, 40))
 
-#     # event 1123 - 1126 # i think here there is a duplicate that doesn't make sense, 1126 + 1124
-#     #hits.append(Hit(5, 6, 23))
-#     hits.append(Hit(3, 6, 23))
-#     #hits.append(Hit(4, 0, 7))
-#     hits.append(Hit(2, 1, 7))
-#     hits.append(Hit(7, 1, 25))
-#     #hits.append(Hit(6, 7, 40))
-#     hits.append(Hit(1, 1, 25))
-#     hits.append(Hit(0, 6, 40))
+    if event == 1048:
+        hits.append(Hit(5, 6, 19))
+        hits.append(Hit(3, 6, 19))
+        hits.append(Hit(7, 1, 25))
+        hits.append(Hit(1, 1, 25))
+        hits.append(Hit(0, 6, 40))
 
-#     # event 1048
-#     hits.append(Hit(5, 6, 19))
-#     hits.append(Hit(3, 6, 19))
-#     #hits.append(Hit(4, 0, 7))
-#     #hits.append(Hit(2, 1, 3))
-#     hits.append(Hit(7, 1, 25))
-#     #hits.append(Hit(6, 7, 40))
-#     hits.append(Hit(1, 1, 25))
-#     hits.append(Hit(0, 6, 40))
+    if event == 1572:
+        hits.append(Hit(5, 7, 51))
+        hits.append(Hit(3, 7, 51))
+        hits.append(Hit(4, 0, 35))
+        hits.append(Hit(6, 7, 40))
+        hits.append(Hit(1, 0, 25))
+        hits.append(Hit(0, 7, 40))
 
-
-#     hits.append(Hit(7, 0, 25))
-#     hits.append(Hit(6, 7, 40))
-#     hits.append(Hit(5, 7, 47))
-#     hits.append(Hit(4, 0, 31))
-#     hits.append(Hit(3, 7, 47))
-#     hits.append(Hit(2, 0, 31))
-#     hits.append(Hit(1, 0, 25))
-#     hits.append(Hit(0, 7, 40))
 
     my_roads = create_roads(512)
 
@@ -149,7 +157,8 @@ def main():
         nv = 0
         road_good = True
         for hit in hits:
-            strip = hit.ivmm * 64 + hit.ich 
+            #print hit.ivmm, hit.ich, hit.ib
+            strip = hit.ivmm * 64 + hit.ich - 1
             strip = flip(strip, hit.ib)
             if not road.contains_hit(hit.ib, strip):
                 road_good = False
@@ -157,7 +166,7 @@ def main():
         if road_good:
             print
             print "road: (%d,%d,%d)" %(road.iroadx,road.iroadu,road.iroadv)
-
+            print "margin", road.margin_boards
 #             if road.contains_hit(hit.ib, strip):
 #                 if hit.ib < 2 or hit.ib > 5:
 #                     nx += 1
